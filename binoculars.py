cbd644773dfd0062bee47396fb59d93928cae75b
ChrisNassif <chrisjnassif@gmail.com>
2024-09-14 08:43:33 -0400

    Fixed bug where models would consume all the VRAM on the host machine

--- binoculars.py
+++ binoculars.py
@@ -6,10 +6,14 @@ import torch
 from typing import Tuple
 import re
 
-BINOCULARS_MODEL_PERFORMER_NAME = "google/gemma-2-2b-it"
-BINOCULARS_MODEL_OBSERVER_NAME = "google/gemma-2-2b"
+# BINOCULARS_MODEL_PERFORMER_NAME = "google/gemma-2-2b-it"
+# BINOCULARS_MODEL_OBSERVER_NAME = "google/gemma-2-2b"
 
-QUANTIZATION_CONFIG = QuantoConfig(weights="float8")
+BINOCULARS_MODEL_PERFORMER_NAME = "HuggingFaceTB/SmolLM-360M-Instruct"
+BINOCULARS_MODEL_OBSERVER_NAME = "HuggingFaceTB/SmolLM-360M"
+
+
+QUANTIZATION_CONFIG = QuantoConfig(weights="int4")
 
 BINOCULARS_ACCURACY_THRESHOLD = 0.9015310749276843 
 BINOCULARS_FPR_THRESHOLD = 0.8536432310785527 
@@ -37,6 +41,7 @@ class Binoculars:
             self.compute_log_cross_perplexity(reference_text, tokenizer, observer_model, performer_model, device))
     
     
+    @torch.no_grad()
     def compute_log_perplexity(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, reference_text: str, device):
         
         reference_text_tokens = tokenizer(reference_text, return_tensors="pt").to(device)
@@ -54,14 +59,16 @@ class Binoculars:
             next_token_logits = outputs.logits[:, -1, :]
             next_tokens_logits_softmax = torch.softmax(next_token_logits, dim=-1)
             
-            total_log_likelihood -= torch.log(next_tokens_logits_softmax[:,token])
-            # print(total_log_likelihood)
+            log_probs = torch.log(next_tokens_logits_softmax[:,token])
+            print(f"not cross: {log_probs}")
+            total_log_likelihood -= log_probs
             
             word_count += 1
             
         return total_log_likelihood / word_count
             
             
+    @torch.no_grad()    
     def compute_log_cross_perplexity(
         self, reference_text: str,
         tokenizer: AutoTokenizer,
@@ -71,36 +78,37 @@ class Binoculars:
         ):
         
         reference_text_tokens = tokenizer(reference_text, return_tensors="pt").to(device)
-        eos_token_id = tokenizer.eos_token_id
+        context_tokens = tokenizer.encode("", return_tensors="pt").to(device).type(torch.int32)
         
-        total_log_likelihood = 0
+        total_cross_entropy = 0
         word_count = 0
 
-        context_tokens = tokenizer.encode("", return_tensors="pt").to(device).type(torch.int32)
         # print(tokenizer.encode("The", return_tensors="pt").to(device).type(torch.int32))
         for token in reference_text_tokens['input_ids'][0]:
             
             context_tokens = torch.cat([context_tokens, token.reshape(1, 1)], dim=-1)
+            
             
             performer_outputs = performer_model(context_tokens)
             observer_outputs = observer_model(context_tokens)
     
             performer_next_token_logits = performer_outputs.logits[:, -1, :]
             observer_next_token_logits = observer_outputs.logits[:, -1, :]
-            
-            # print(observer_outputs.logits)
-            
+
             performer_next_tokens_logits_softmax = torch.softmax(performer_next_token_logits, dim=-1)
             observer_next_token_logits_softmax = torch.softmax(observer_next_token_logits, dim=-1)
             
-            # print(performer_next_tokens_logits_softmax)
-            # print(torch.log(observer_next_token_logits_softmax))
-            total_log_likelihood -= torch.dot(performer_next_tokens_logits_softmax[-1], torch.log(observer_next_token_logits_softmax[-1])) 
-            # print(total_log_likelihood)
+            # print(performer_next_tokens_logits_softmax[-1])
+            # print(observer_next_token_logits_softmax[-1])
+            
+            cross_entropy = torch.dot(performer_next_tokens_logits_softmax[-1], torch.log(observer_next_token_logits_softmax[-1])) 
+            print(f"cross: {cross_entropy}")
+            total_cross_entropy -= cross_entropy
             
             word_count += 1
             
-        return total_log_likelihood / word_count
+            
+        return total_cross_entropy / word_count
         
         
     
@@ -148,10 +156,17 @@ if __name__ == "__main__":
     hugging_face_auth_token = get_hugging_face_auth_token("hugging_face_auth_token.txt")
     binoculars = Binoculars(BINOCULARS_MODEL_OBSERVER_NAME, BINOCULARS_MODEL_PERFORMER_NAME, hugging_face_auth_token)
     
-    print(binoculars.predict(
-        "Central to Hitler's ideology was the belief in racial purity and the superiority of the so-called Aryan race. He sought to create a homogeneous, racially pure German society by promoting policies that encouraged the reproduction of Aryan individuals while eliminating those he deemed racially inferior. This led to the promotion of eugenics, a pseudo-scientific movement aimed at improving the genetic quality of the human population through selective breeding. The Nazis sterilized thousands of people with disabilities", 
+    SENTENCE = "Roses are one of the most iconic and beloved flowers in the world, admired for their beauty, fragrance, and deep symbolism. For centuries, they have been a symbol of love, passion, beauty, and even secrecy, making them significant in various cultures and historical periods. From literature and art to gardens and personal moments, roses have held a special place in human history. This essay explores the history, symbolism, and enduring appeal of the rose, as well as its significance in art, culture, and nature."
+    binoculars_score = binoculars.predict(
+        SENTENCE, 
         binoculars.performer_tokenizer, 
         binoculars.observer_model, 
         binoculars.performer_model,
         "cuda:0",
-    ))
+    )
+    print(binoculars_score)
+    
+    if binoculars_score > 4:
+        print("AI Generated")
+    else:
+        print("Not AI Generated")
